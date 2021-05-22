7d94fa26da3ceea43ce6fcf15b9050fa24ba7ce4
Juan Rojas <jrojasbf@gmail.com>
2021-05-22 02:36:30 +0000

    fixed token, added  and

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}').format(client)
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -17,5 +17,12 @@ async def on_message(message):
 
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
+
+  if message.content.startswith('$commands'):
+    await message.channel.send('View all commands here - https://github.com/jrojas10/Mecha-Senku\n ```$github - links to github\n$hello - replies with hello\n$commands- shows all commands```')
+
+  if message.content.startswith('$github'):
+    await message.channel.send('View the source code and commands\nhttps://github.com/jrojas10/Mecha-Senku')
+
 
 client.run(my_secret)
