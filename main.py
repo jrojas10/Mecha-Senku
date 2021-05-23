de0148fb9e4dc74b2e713aba5b77d4f453921080
Juan Rojas <jrojasbf@gmail.com>
2021-05-23 07:03:59 +0000

    added quote and aniquote commands

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import os
 import discord
+import requests
+import json
 
 my_secret = os.environ['token']
 
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] +  " -" + json_data[0]['a']
+  return quote
+
+def get_aniquote():
+  response = requests.get("https://animechan.vercel.app/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data['quote'] + " -" + json_data['character'] + " ("+json_data['anime'] +")"
+  return quote
+
 
 @client.event
 async def on_ready():
@@ -16,7 +31,7 @@ async def on_message(message):
     return
 
   if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    await message.channel.send('Hello!, my name is Mecha Senku')
 
   if message.content.startswith('$commands'):
     await message.channel.send('View all commands here - https://github.com/jrojas10/Mecha-Senku\n ```$github - links to github\n$hello - replies with hello\n$commands- shows all commands```')
@@ -24,5 +39,15 @@ async def on_message(message):
   if message.content.startswith('$github'):
     await message.channel.send('View the source code and commands\nhttps://github.com/jrojas10/Mecha-Senku')
 
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if message.content.startswith('$aniquote'):
+    quote = get_aniquote()
+    await message.channel.send(quote)
+
+
+  
 
 client.run(my_secret)
